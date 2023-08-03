a8c1e9fa35ee8b3d5a3f645804733e64e683fcb2
Kishore S <87610640+KISHORExlr8@users.noreply.github.com>
2023-08-03 20:58:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 20;
+const c = 30;
+
+if (a > b && a > c) {
+  console.log("A is greater");
+} else if (b > c) {
+  console.log("B is greater");
+} else {
+  console.log("C is greater");
+}
+
+const hobbies = ["Anime", "Sports", "YouTube"];
+
+for (const hobby of hobbies) {
+  console.log(hobby);
+}
